f0baaed2be43b02114bb4101a935bc9aed4fe2f3
Hemant Singh <101858977+HemantS404@users.noreply.github.com>
2024-05-12 15:40:50 +0530

    Update VernamCipher.py

--- IS/VernamCipher.py
+++ IS/VernamCipher.py
@@ -1,6 +1,16 @@
-text = "HELLO".upper()
-key = "MEOWY".upper()
+text = "RAMSWARUPK".upper()
+key = "RANCHOBABA".upper()
+print(f"Plain Text : {text}")
+print(f"Key Text : {key}")
 
 cipher = ""
 for i in range(len(text)):
-    ctext = (ord(text[i]) - ord("A"))^(ord(key[i]) - ord("A"))
+    ctext = chr(((ord(text[i]) - ord("A"))^(ord(key[i]) - ord("A")))%26 + ord("A"))
+    cipher += ctext
+print(f"Cipher Text : {cipher}")
+
+decipher = ""
+for i in range(len(cipher)):
+    dtext = chr(((ord(cipher[i]) - ord("A"))^(ord(key[i]) - ord("A")))%26 + ord("A"))
+    decipher += dtext
+print(f"Decipher Text : {decipher}")
